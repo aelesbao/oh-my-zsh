e22793c9ca7376c9c84c49aad0479c14cf2f21fb
Augusto Rocha Elesbão <aelesbao@gmail.com>
2012-07-27 11:01:34 -0300

    Now updates checks for existance of remote tracking branch "robbyrussell"

--- tools/update_fork.sh
+++ tools/update_fork.sh
@@ -1,6 +1,7 @@
-if git fetch robbyrussell master; then
-  echo "Merging with robbyrussell/master"
-  git merge robbyrussell/master
-fi
+git pull
 
-git stash && git pull && git push
+robbyrussell_remote_track=$(git remote | grep -o 'robbyrussell')
+if [ -n "${robbyrussell_remote_track}" ]; then
+  echo "Merging with robbyrussell/master"
+  git fetch -q robbyrussell master && git merge robbyrussell/master && git push -q origin master
+fi
