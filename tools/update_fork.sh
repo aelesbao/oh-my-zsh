dd0592c28213e9e4326c23ea1742b84d1567c546
Augusto Rocha Elesbão <aelesbao@gmail.com>
2012-08-27 10:42:09 -0300

    Added more stats infos to update_fork script.

--- tools/update_fork.sh
+++ tools/update_fork.sh
@@ -1,7 +1,32 @@
-git pull
+git_has_upstream_diff() {
+  local STATUS=1
 
-robbyrussell_remote_track=$(git remote | grep -o 'robbyrussell')
-if [ -n "${robbyrussell_remote_track}" ]; then
-  echo "Merging with robbyrussell/master"
-  git fetch -q robbyrussell master && git merge robbyrussell/master && git push -q origin master
+  # find how many commits we are ahead/behind our upstream
+  local COUNT=$(git rev-list --count --left-right @{upstream}...HEAD 2> /dev/null)
+  case "$COUNT" in
+    "0	"*) # ahead of upstream
+      STATUS=2 ;;
+    *"	0") # behind upstream
+      STATUS=-2 ;;
+    *) # diverged from upstream
+      STATUS=0 ;;
+  esac
+
+  return $STATUS
+}
+
+git pull --rebase
+
+remote_track=$(git remote | grep -o 'robbyrussell')
+if [ -n "${remote_track}" ]; then
+  echo "Merging with ${remote_track}/master"
+  (git fetch ${remote_track} master && git merge ${remote_track}/master) || {
+    echo "Failed to integrate changes on ${remote_track}"
+    exit -1
+  }
+fi
+
+if git_has_upstream_diff; then
+  echo "Updating origin/master"
+  git push origin master
 fi
