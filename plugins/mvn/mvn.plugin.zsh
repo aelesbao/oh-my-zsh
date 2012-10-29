a5e9cbbc12eb17f2d4ed6023cdf596a9c633e5c7
Augusto Rocha Elesbão <aelesbao@gmail.com>
2012-10-29 12:41:32 -0200

    Added android completition maps to mvn plugin

--- plugins/mvn/mvn.plugin.zsh
+++ plugins/mvn/mvn.plugin.zsh
@@ -1,6 +1,14 @@
-function listMavenCompletions { 
-	 reply=(
-		cli:execute cli:execute-phase archetype:generate generate-sources compile clean install test test-compile deploy package cobertura:cobertura jetty:run gwt:run gwt:debug -DskipTests -Dmaven.test.skip=true -DarchetypeCatalog=http://tapestry.formos.com/maven-snapshot-repository -Dtest= `if [ -d ./src ] ; then find ./src -type f | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi`); 
+function listMavenCompletions {
+  reply=(
+    cli:execute cli:execute-phase archetype:generate generate-sources compile clean install
+    test test-compile deploy package cobertura:cobertura jetty:run gwt:run gwt:debug
+    android:apk android:apklib android:deploy android:deploy-dependencies android:dex android:emulator-start
+    android:emulator-stop android:emulator-stop-all android:generate-sources android:help android:instrument
+    android:manifest-update android:pull android:push android:redeploy android:run android:undeploy
+    android:unpack android:version-update android:zipalign android:devices
+    -DskipTests -Dmaven.test.skip=true -DarchetypeCatalog=http://tapestry.formos.com/maven-snapshot-repository
+    -Dtest= `if [ -d ./src ] ; then find ./src -type f | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi`
+  );
 }
 
 compctl -K listMavenCompletions mvn
