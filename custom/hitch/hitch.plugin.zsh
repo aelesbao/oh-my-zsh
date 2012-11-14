21868261be1b8547466a7e63a47111ee05aebbfd
Augusto Rocha Elesbão <aelesbao@gmail.com>
2012-11-14 17:26:21 -0200

    Fixed hitch plugin

--- custom/hitch/hitch.plugin.zsh
+++ custom/hitch/hitch.plugin.zsh
@@ -1,10 +1,10 @@
 # hitch: tool to edit authors when in pair programming
 if [ -n "`command -v hitch`" ]; then
-  hitch() {
+  _hitch() {
     command hitch "$@"
     if [[ -s "$HOME/.hitch_export_authors"  ]] ; then source "$HOME/.hitch_export_authors" ; fi
   }
   alias unhitch='hitch -u'
   # Uncomment to persist pair info between terminal instances
-  hitch
+  alias hitch=_hitch
 fi
